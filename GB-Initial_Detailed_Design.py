a7f96ef612541ea253cdc8a898774191860b3d3d
hfall123 <hfall009@uottawa.ca>
2025-11-11 12:21:14 -0500

    Update GB-Initial_Detailed_Design.py

---
+++ GB-Initial_Detailed_Design.py
@@ -0,0 +1,7 @@
+#function to move the servos to a specific location using inverse kinematics
+
+#function to correct any servo control errors
+
+#function to move the servo arm a certain degree
+
+#function to read PWM singals from the knobs
