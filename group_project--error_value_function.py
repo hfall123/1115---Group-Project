33de204bef0bfbf3c172e00b31414129a75b88cb
hfall123 <hfall009@uottawa.ca>
2025-11-17 15:36:56 -0500

    Update group_project--error_value_function.py

---
+++ group_project--error_value_function.py
@@ -0,0 +1,9 @@
+#function to check accuracy of the servo
+def error_value(expected_degree, actual_degree):
+    #checks the angle value that the servo thinks it is at
+
+    #checks the actual angle value from the encoder
+
+    #subtracts the actual from the expected value to get the difference (error value)
+
+    return #this will return the error value
