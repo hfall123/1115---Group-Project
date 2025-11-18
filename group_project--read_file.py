8d42e1af7b3aa36bcc31a7ae242029a651f21d1c
cam23456 <aruta095@uottawa.ca>
2025-11-17 19:42:18 -0500

    read_file()

---
+++ group_project--read_file.py
@@ -0,0 +1,6 @@
+#function to read and parse the file for error testing (Amanda)
+def read_file(): 
+    #retrieve error testing coordinates form a separate file (testing every 10 degrees)
+    #read, strip and split the file
+    #add a time buffer so that the servo has time to complete the tasks and doesnt get overwhelmend
+    return #returns stripped and split file
