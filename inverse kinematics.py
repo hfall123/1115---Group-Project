6ced9d41cfbbbf65bfd9f2a8d166ad98a0bccca8
hfall123 <hfall009@uottawa.ca>
2025-12-01 18:57:56 -0500

    Update inverse kinematics.py

--- inverse kinematics.py
+++ inverse kinematics.py
@@ -1,22 +1,23 @@
-def inverse_kinematics(Cx, Cy, La, Lb, theta_S_offset=0, theta_E_offset=0):
-    # angle AC
-    angle_AC = math.atan2(Cx, Cy)
-    
-    # length AC
-    AC = math.sqrt(Cy**2 + Cx**2)
-    
-    # angle BAC
-    angle_BAC = math.acos((La**2 + (AC**2) - Lb**2) / (2 * La * AC))
-    
-    # angle ABC
-    angle_ABC = math.acos((La**2 + AC**2 - Lb**2) / (2 * La * Lb))
-    
-    # theta_AB
-    theta_AB = angle_AC - angle_BAC
-    
-    # shoulder and elbow angles (with offsets)
-    theta_S = theta_S_offset + theta_AB
-    theta_E = angle_ABC - theta_E_offset
-    
-    # convert to degrees for servo control
-    return math.degrees(theta_S), math.degrees(theta_E)
+#function that gives you the proper wrist location based off of information about the angles of the servo and arm lengths
+def inverse_kinematics(wrist_x, wrist_y): 
+    try:
+        angle_AC = math.atan2(wrist_y,wrist_x)
+        AC = math.sqrt(wrist_y **2 + wrist_x **2)
+        angle_BAC = math.acos( (L1 ** 2 + (AC **2) - L2 **2)/ (2*L1*AC))
+        angle_ABC = math.acos((L1**2 + AC**2 - L2**2)/(2*L1*L2))
+        #find the angles each servo should be at
+        theta_ab = angle_AC - angle_BAC
+        servo_shoulder = o_s_off + theta_ab
+        servo_elbow = angle_ABC - o_e_off
+
+        servo_shoulder_deg = math.degrees(servo_shoulder)
+        servo_elbow_deg = math.degrees(servo_elbow)
+
+        #convert the resulting angles to degrees and find the correct angle for the servos
+    except:
+        #notfiy the user if there is an issue with completingt the calculations
+        print("there has been an issue caluclating the proper anlge values using reverse kinematics")
+
+    #return the angles that the servos should be at in order to reach the specified wrist coordinates
+    return servo_shoulder_deg, servo_elbow_deg
+
