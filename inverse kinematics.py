bdcd1a70eb34f069adf2f93abdb6b7dc851ae7ab
juliette <juliette.m.coelho@gmail.com>
2025-12-01 16:22:15 -0500

    Refactor inverse kinematics function with offsets

--- inverse kinematics.py
+++ inverse kinematics.py
@@ -1,25 +1,22 @@
-import math
-
-# computes shoulder and elbow servo angles required to move pen to desired position
-def inverse_kinematics(x, y, L1, L2):
-    # distance from shoulder base to target point
-    r = math.sqrt(x*x + y*y)
-
-    # checks reachability / mechanical limits
-    if r > (L1 + L2) or r < abs(L1 - L2):
-        return None  # point is physically impossible to reach
-
-    # elbow angle
-    cos_elbow = (r*r - L1*L1 - L2*L2) / (2 * L1 * L2)
-    theta_elbow = math.acos(cos_elbow)
-
-    # shoulder angle
-    k1 = L1 + L2 * math.cos(theta_elbow)
-    k2 = L2 * math.sin(theta_elbow)
-    theta_shoulder = math.atan2(y, x) - math.atan2(k2, k1)
-
-    # converts to degrees
-    theta_shoulder_deg = math.degrees(theta_shoulder)
-    theta_elbow_deg    = math.degrees(theta_elbow)
-
-    return theta_shoulder_deg, theta_elbow_deg
+def inverse_kinematics(Cx, Cy, La, Lb, theta_S_offset=0, theta_E_offset=0):
+    # angle AC
+    angle_AC = math.atan2(Cx, Cy)
+    
+    # length AC
+    AC = math.sqrt(Cy**2 + Cx**2)
+    
+    # angle BAC
+    angle_BAC = math.acos((La**2 + (AC**2) - Lb**2) / (2 * La * AC))
+    
+    # angle ABC
+    angle_ABC = math.acos((La**2 + AC**2 - Lb**2) / (2 * La * Lb))
+    
+    # theta_AB
+    theta_AB = angle_AC - angle_BAC
+    
+    # shoulder and elbow angles (with offsets)
+    theta_S = theta_S_offset + theta_AB
+    theta_E = angle_ABC - theta_E_offset
+    
+    # convert to degrees for servo control
+    return math.degrees(theta_S), math.degrees(theta_E)
