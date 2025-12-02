b5c91bd7f15f5996f3d3955f748ab986023c23e2
cam23456 <aruta095@uottawa.ca>
2025-12-02 14:09:14 -0500

    initial code

--- inverse_kin_final_testing.py
+++ inverse_kin_final_testing.py
@@ -1,1 +1,34 @@
+#function that gives you the proper wrist location based off of information about the angles of the servo and arm lengths
+def inverse_kinematics(wrist_x, wrist_y):
+    
+    #define bourdries so that the user cannot draw outside of the bounries of the page
+    if wrist_x > 20:
+        wrist_x = 20
+    elif wrist_y < 20:
+        wrist_y = 20
+    
+    # Distance from shoulder to target
+    AC = math.sqrt(wrist_x**2 + wrist_y**2)
+
+    #ensure there are no math errors as a result of division by 0
+    if AC == 0:
+        AC = 1
+
+    #Angle from shoulder to target
+    angle_AC = math.atan2(wrist_y, wrist_x)
+    #Law of cosines for triangle at shoulder
+    angle_BAC = math.acos((L1**2 + AC**2 - L2**2) / (2 * L1 * AC))
+    #Shoulder angle
+    theta_shoulder = angle_AC - angle_BAC
+    #Law of cosines for elbow
+    angle_elbow = math.acos((L1**2 + L2**2 - AC**2) / (2 * L1 * L2))
+    theta_elbow = math.pi - angle_elbow  # elbow bends "down"
+    #Apply servo offsets
+    theta_shoulder += o_s_off
+    theta_elbow += o_e_off
+    #Convert to degrees
+    shoulder_deg = math.degrees(theta_shoulder) - 80
+    elbow_deg = math.degrees(theta_elbow)
+
+    return shoulder_deg, elbow_deg
 
