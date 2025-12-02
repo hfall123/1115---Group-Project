293fc3857c9e9b4efe7e0ddfd537aec1a960c1ef
hfall123 <hfall009@uottawa.ca>
2025-12-02 07:49:00 -0500

    Update inverse kinematics.py

--- inverse kinematics.py
+++ inverse kinematics.py
@@ -1,23 +1,34 @@
-#function that gives you the proper wrist location based off of information about the angles of the servo and arm lengths
-def inverse_kinematics(wrist_x, wrist_y): 
-    try:
-        angle_AC = math.atan2(wrist_y,wrist_x)
-        AC = math.sqrt(wrist_y **2 + wrist_x **2)
-        angle_BAC = math.acos( (L1 ** 2 + (AC **2) - L2 **2)/ (2*L1*AC))
-        angle_ABC = math.acos((L1**2 + AC**2 - L2**2)/(2*L1*L2))
-        #find the angles each servo should be at
-        theta_ab = angle_AC - angle_BAC
-        servo_shoulder = o_s_off + theta_ab
-        servo_elbow = angle_ABC - o_e_off
+def inverse_kinematics(wrist_x, wrist_y):
+    # Distance from shoulder to target
+    AC = math.sqrt(wrist_x**2 + wrist_y**2)
 
-        servo_shoulder_deg = math.degrees(servo_shoulder)
-        servo_elbow_deg = math.degrees(servo_elbow)
+    # --- Check reachability ---
+    if AC == 0:
+        AC = 1
 
-        #convert the resulting angles to degrees and find the correct angle for the servos
-    except:
-        #notfiy the user if there is an issue with completingt the calculations
-        print("there has been an issue caluclating the proper anlge values using reverse kinematics")
+    # Angle from shoulder to target
+    angle_AC = math.atan2(wrist_y, wrist_x)
+    
 
-    #return the angles that the servos should be at in order to reach the specified wrist coordinates
-    return servo_shoulder_deg, servo_elbow_deg
+    # Law of cosines for triangle at shoulder
+    angle_BAC = math.acos((L1**2 + AC**2 - L2**2) / (2 * L1 * AC))
+
+    # Shoulder angle
+    theta_shoulder = angle_AC + angle_BAC
+
+    # Law of cosines for elbow
+    angle_elbow = math.acos((L1**2 + L2**2 - AC**2) / (2 * L1 * L2))
+    
+    theta_elbow = angle_elbow - math.pi  # elbow bends "down"
+
+    # Apply servo offsets
+    theta_shoulder += o_s_off
+    theta_elbow += o_e_off
+
+    # Convert to degrees
+    shoulder_deg = math.degrees(theta_shoulder)
+    elbow_deg = math.degrees(theta_elbow)
+
+    return shoulder_deg, elbow_deg
+
 
